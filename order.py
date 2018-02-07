377ddd73c4c2eb00bd8e29e440e5bc0eaedba38c
akidan <zhiyue7@gmail.com>
2018-02-08 01:34:40 +0900

    add confirmation and error trace

--- order.py
+++ order.py
@@ -19,8 +19,18 @@ if argc > 1 and argvs[1]=="-t" and argc==6:
     mona_amount=int(argvs[3])
     mona_limit =float(argvs[4])
     mona_stop  =float(argvs[5])
-    tranx = zaif.create_position(type='margin',currency_pair='mona_jpy',action='ask',amount=mona_amount,price=mona_price,limit=mona_limit,stop=mona_stop,leverage=1)
-    print(tranx)
+    print ("\nPrice="+str(mona_price)+", Amount="+str(mona_amount)+", (Total="+str(mona_price*float(mona_amount))+"), Limit="+str(mona_limit)+", Stop="+str(mona_stop))
+    print ("Are you sure? [Yn]")
+    confirm = input()
+    if confirm == "Y":
+        try:
+            order = zaif.create_position(type='margin',currency_pair='mona_jpy',action='ask',amount=mona_amount,price=mona_price,limit=mona_limit,stop=mona_stop,leverage=1)
+            print(order)
+            print("Order success!")
+        except:
+            import traceback
+            traceback.print_exc()
+            print("Order error!!")
 elif argc > 1 and argvs[1]=="-c" and argc==3:
     mona_leverage_id = int(argvs[2])
     print(zaif.cancel_position(type='margin',leverage_id=mona_leverage_id))
