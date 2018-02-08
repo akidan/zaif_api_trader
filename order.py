fc2cb05650e40fd25920db211374650e68910d42
zhiyue.wang <zhiyue.wang@rakuten.com>
2018-02-08 09:56:22 +0900

    improve input experience

--- order.py
+++ order.py
@@ -5,8 +5,8 @@ from zaifapi import *
 argvs = sys.argv
 argc = len(argvs)
 
-zaif = ZaifPublicApi()
-print(zaif.last_price('mona_jpy'))
+zaif_pub = ZaifPublicApi()
+print(zaif_pub.last_price('mona_jpy'))
 
 json_str = os.popen("cat ~/.secrets/key.json").read()
 json_dict=json.loads(json_str)
@@ -14,14 +14,34 @@ key=json_dict['zaif']['key']
 secret=json_dict['zaif']['secret']
 zaif = ZaifLeverageTradeApi(key, secret)
 
-if argc > 1 and argvs[1]=="-t" and argc==6:
-    mona_price =float(argvs[2])
-    mona_amount=int(argvs[3])
-    mona_limit =float(argvs[4])
-    mona_stop  =float(argvs[5])
+if argc > 1 and argvs[1]=="-t":
+    if argc < 3:
+        print ("Input price:")
+        mona_price = float(input())
+    else:
+        mona_price = float(argvs[2])
+    if argc < 4:
+        print ("Input amount:")
+        mona_amount = int(input())
+    else:
+        mona_amount = int(argvs[3])
+    if argc < 5:
+        print ("Input limit:")
+        mona_limit = float(input())
+    else:
+        mona_limit =float(argvs[4])
+    if argc < 6:
+        print ("Input stop:")
+        mona_stop = float(input())
+    else:
+        mona_stop  =float(argvs[5])
+
     print ("\nPrice="+str(mona_price)+", Amount="+str(mona_amount)+", (Total="+str(mona_price*float(mona_amount))+"), Limit="+str(mona_limit)+", Stop="+str(mona_stop))
-    print ("Are you sure? [Yn]")
-    confirm = input()
+    confirm = ""
+    while confirm == "":
+        print(zaif_pub.last_price('mona_jpy'))
+        print("Are you sure? [Yn]")
+        confirm = input()
     if confirm == "Y":
         try:
             order = zaif.create_position(type='margin',currency_pair='mona_jpy',action='ask',amount=mona_amount,price=mona_price,limit=mona_limit,stop=mona_stop,leverage=1)
