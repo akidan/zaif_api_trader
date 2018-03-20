44af0d96536b977e5b214b519406b809497df939
zhiyue.wang <zhiyue.wang@rakuten.com>
2018-03-20 18:58:58 +0900

    improve for bid and ask

--- order.py
+++ order.py
@@ -19,9 +19,11 @@ class Trader(object):
     def __init__(self):
         return None
 
-    def trade(self, c_type, c_amount, c_price):
+    def trade(self, c_type, c_amount, c_price, c_action):
         try:
-            order = zaif.trade(currency_pair=c_type+'_jpy',action='ask',amount=c_amount,price=c_price)
+            if c_type == 'btc' or c_type == 'eth':
+                c_price = int(c_price)
+            order = zaif.trade(currency_pair=c_type+'_jpy',action=c_action,amount=c_amount,price=c_price)
             print(order)
             print("Order success!")
             return False
@@ -31,14 +33,12 @@ class Trader(object):
             print("Order error!!")
             return True
 
-if argc > 2:
-    coin_type = str(argvs[2])
-
-print(coin_type)
-print(zaif_pub.last_price(coin_type+'_jpy'))
-
-if argc > 1 and argvs[1] == "-t":
-    if argc == 2:
+if argc > 1 and (argvs[1] == "-b" or argvs[1] == "-a"):
+    if argvs[1] == "-b":
+        coin_action = 'bid'
+    elif argvs[1] == "-a":
+        coin_action = 'ask'
+    if argc < 3:
         print ("Input coin:")
         coin_type = str(input())
         print(zaif_pub.last_price(coin_type+'_jpy'))
@@ -49,16 +49,16 @@ if argc > 1 and argvs[1] == "-t":
         coin_price = float(argvs[3])
     if argc < 5:
         print ("Input amount:")
-        coin_amount = int(input())
+        coin_amount = float(input())
     else:
-        coin_amount = int(argvs[4])
+        coin_amount = float(argvs[4])
     if argc > 5 and argvs[5] == "F":
         confirm = "Y"
     elif argc > 5 and argvs[5] == "FF":
         confirm = "FF"
     else:
     	confirm = ""
-    print ("\nCoin="+coin_type+", Price="+str(coin_price)+", Amount="+str(coin_amount)+", (Total="+str(coin_price*float(coin_amount))+")")
+    print ("\nCoin="+coin_type+", Action="+coin_action+", Price="+str(coin_price)+", Amount="+str(coin_amount)+", (Total="+str(coin_price*float(coin_amount))+")")
     while confirm == "":
         print(zaif_pub.last_price(coin_type+'_jpy'))
         print("Are you sure? [YnFF]")
@@ -68,15 +68,19 @@ if argc > 1 and argvs[1] == "-t":
     if confirm == "FF":
         try_num = 10
     fail = True
-    while (fail and try_num > 0 and confirm == "Y" or confirm == "FF"):
-        fail = trader.trade(coin_type, coin_amount, coin_price)
+    while (fail and try_num > 0 and (confirm == "Y" or confirm == "FF")):
+        fail = trader.trade(coin_type, coin_amount, coin_price, coin_action)
         try_num = try_num - 1
         sleep(1)
 
 elif argc > 1 and argvs[1]=="-c" and argc==3:
-    coin_leverage_id = int(argvs[2])
-    print(zaif.cancel_position(type='margin',leverage_id=coin_leverage_id))
-elif argc > 1 and argvs[1]=="-a":
-    print(zaif.active_positions(type='margin',currency_pair='xem_jpy'))
+    coin_order_id = int(argvs[2])
+    print(zaif.cancel_order(order_id=coin_order_id))
+elif argc > 1 and argvs[1]=="-l":
+    print(zaif.active_orders())
+elif argc > 1 and argvs[1] in ["btc", "eth", "xem", "mona", "zaif"]:
+    coin_type = str(argvs[1])
+    print(coin_type+" "+str(zaif_pub.last_price(coin_type+'_jpy')))
 else:
-    print("Usage: \npython order.py -t [coin] [price] [amount] [F|FF]\npython order.py -c [trade_id]\npython order.py -a")
+    print(coin_type+" "+str(zaif_pub.last_price(coin_type+'_jpy')))
+    print("Usage: \npython order.py -b -a [coin] [price] [amount] [F|FF]\n                -c    [trade_id]\n                -l")
